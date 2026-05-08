427f3c0cab31b5142d0662a26c42c2753bb20bf3
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-08 01:42:07 +0100

    chore: ignore unnecessary files and directories

--- test_green2.py
+++
@@ -1,7 +0,0 @@
-import os
-if os.name == 'nt':
-    os.system('color')
-print('\033[32m', end='')
-print("This should be green.")
-print("This too.")
-print('\033[0m', end='')
--- test_green3.py
+++
@@ -1,9 +0,0 @@
-import os
-import atexit
-
-if os.name == 'nt':
-    os.system('color')
-print('\033[32m', end='')
-atexit.register(lambda: print('\033[0m', end=''))
-
-print("This is green.")
