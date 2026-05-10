bb7af7192f0a6b404de08a9df1264eacd1ed545a
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-10 17:04:03 +0100

    edit

--- use_of_class/car.py
+++ use_of_class/car.py
@@ -5,6 +5,6 @@ class Car:
         self.color = color
         self.for_sale = for_sale
     def drive(self):
-        print("You drive the car")
+        print(f"You drive the {self.model}")
     def stop(self):
-        print("You stop the car")
+        print(f"You stop the {self.model}")
