5d67798b16473724d7c224126bcdf8768ac3d83f
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-10 16:26:03 +0100

    edit

--- hangman/wordlist.py
+++ hangman/wordlist.py
@@ -2,5 +2,5 @@
 
 words = ("apple", "orange", "banana", "coconut", "pineapple",
          "cashew", "meat", "Pear", "falcon", "frog", "leopard", "salamander",
-         "hyena", " maize", "yellow", "eagle", "cocoyam", "coffee",
+         "hyena", "maize", "yellow", "eagle", "cocoyam", "coffee",
          "octopus", "cheetah", "kangaroo", "giraffe", "flamingo", "Gorilla")
