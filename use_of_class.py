2e5907653ad36146247dd1cca6b2e7a62012f7b2
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-10 16:50:47 +0100

    testing

--- use_of_class.py
+++ use_of_class.py
@@ -5,10 +5,10 @@ class Car:
         self.color = color
         self.for_sale = for_sale
 
-car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Mustang", 2024, "red", False)
 car2 = Car("Corvette", 2025, "blue", True)
 car2 = Car("Charger", 2026, "yellow", True)
-print(car1.model)
-print(car1.year)
-print(car1.color)
-print(car1.for_sale)
+print(car2.model)
+print(car2.year)
+print(car2.color)
+print(car2.for_sale)
