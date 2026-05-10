66074f9d07fe4d893605f4ea69d2ca05836db8c1
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-10 16:49:51 +0100

    edit

--- use_of_class.py
+++ use_of_class.py
@@ -7,7 +7,7 @@ class Car:
 
 car1 = Car("Mustang", 2024, "red", False)
 car2 = Car("Corvette", 2025, "blue", True)
-car2 = Car("Charger", 2025, "blue", True)
+car2 = Car("Charger", 2026, "blue", True)
 print(car1.model)
 print(car1.year)
 print(car1.color)
